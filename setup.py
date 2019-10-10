ffcd0d10b4e3342c39a2215a885f3ab79676a379
Zhuwei Xu <zhuweix8@gmail.com>
2019-10-10 13:17:06 -0400

    Empty package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Recombination_analysis',
+      version='0.1',
+      description='Analyze recombination events using kmers',
+      url='',
+      author='Zhuwei Xu',
+      author_email='zhuweixu8@gmail.com',
+      license='MIT',
+      packages=['recombination_analysis'],
+      zip_safe=False)
